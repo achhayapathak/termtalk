9d58ba88fb25bd410a5da519f2f23dfa7edff1e9
Yash Kathe <yashkathe123@gmail.com>
2024-06-24 11:52:21 -0700

    v1.5.1 issue fix #21 : program now reprompts to enter the port on client side on connection error

--- client/client.js
+++ client/client.js
@@ -7,27 +7,43 @@ const startChat = require('./chat');
 const promptPort = require('./promptPort');
 
 
-promptPort((PORT) => {
-    if(PORT) {
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
+const connectToServer = () => {
 
-        console.log(`Trying to connect to server on PORT ${PORT} ...`);
-
-        const socket = ioClient.connect(`http://127.0.0.1:${PORT}`);
-
-        socket.on('connect', () => {
-            console.log(`Connected to server on PORT ${PORT}`);
-            rl.question('Enter your name: ', name => {
-                console.log(`Welcome, ${name}!`);
-                socket.emit('setUsername', name);
-                startChat(name, socket, rl);
+    promptPort((PORT) => {
+        if(PORT) {
+            const rl = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout
             });
-        });
-    } else {
-        console.log('Failed to connect to server server');
-        return;
-    }
-});
+
+            console.log(`Trying to connect to server on PORT ${PORT} ...`);
+
+            const socket = ioClient.connect(`http://127.0.0.1:${PORT}`);
+
+            // handle successful connections 
+            socket.on('connect', () => {
+                console.log(`Connected to server on PORT ${PORT}`);
+                rl.question('Enter your name: ', name => {
+                    console.log(`Welcome, ${name}!`);
+                    socket.emit('setUsername', name);
+                    startChat(name, socket, rl);
+                });
+            });
+
+            // handle connection error when no server is hosted on user defined port
+            socket.on('connect_error', () => {
+                console.log(`\nNo Server is hosted on this PORT ${PORT}, please try a different port`)
+                socket.close()
+                rl.close()
+                connectToServer()
+            })
+
+        } else {
+            console.log('Failed to connect to server server');
+            return;
+        }
+    });
+
+};
+
+connectToServer()
